6837a3da6cad4a217e59cf5e8c5e3e2bd60855b2
root <root@rguo-home.localdomain>
2022-08-16 22:10:11 +0800

    first commit

---
+++ main.go
@@ -0,0 +1,101 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"time"
+)
+
+type Bar struct{
+	Total  int 
+	Bar    int
+	Timer  time.Duration
+	Tag    string
+	Output   string
+	Percent  int
+	Done        chan int
+	Stop      chan struct{}
+}
+
+
+func (b *Bar)disPlay(){
+	fmt.Printf("\r<%-50s>%8s%d%%%8d/%d", b.Output, "",b.Percent, b.Bar, b.Total)
+	b.Output = ""
+}
+
+func (b *Bar)finsh(){
+	close (b.Stop)
+	fmt.Println()
+	fmt.Printf("everything completed!\n")
+}
+
+func NewBar(total int, timer time.Duration, Tag string)*Bar{
+	if total <= 0 {
+		log.Panic(fmt.Sprintf("total task is %d, can't product bar", total))
+	}
+	if Tag == ""{
+		Tag = "#"
+	}
+	if timer == 0 {
+		timer = time.Millisecond * 200
+	}
+	bar := &Bar{
+		Total: total,
+		Timer: timer,
+		Tag: Tag,
+		Done: make(chan int),
+		Stop: make(chan struct{}),
+	}
+	return bar
+}
+
+func (b *Bar)timer(){
+	timer := time.NewTicker(b.Timer)
+	for {
+		select{
+		case <-timer.C:
+			if b.Bar >= b.Total{
+				b.Percent = 100
+				for i :=0; i<= b.Percent/2;i++{
+					b.Output = b.Output + b.Tag
+				}
+				b.disPlay()
+				return
+			}
+			b.Percent = b.Bar * 100 / b.Total
+			for i :=0; i<= b.Percent/2;i++{
+				b.Output = b.Output + b.Tag
+			}
+			b.disPlay()
+		}
+	}
+}
+
+func (b *Bar)input(){
+	for{
+		select{
+		case <- b.Done:
+			b.Bar++
+		case <- b.Stop:
+			return
+	}
+}
+}
+
+func (b *Bar)Run(){
+	go b.input()
+	b.timer()
+	b.finsh()
+}
+
+func main(){
+
+	bar := NewBar(100, time.Microsecond * 200, "#")
+	go bar.Run()
+	for i := 0; i < 100; i++{
+		time.Sleep(100 * time.Millisecond)
+		bar.Done <- i
+	}
+	time.Sleep(time.Second * 1)
+	fmt.Printf("every thing ready!\n")
+}
